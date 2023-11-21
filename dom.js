2a0e535f9c903fac8ce960ae723e1c87de0615d7
rishavece <rishavk229>
2023-11-21 08:42:26 +0530

    send buttton

--- dom.js
+++ dom.js
@@ -1,4 +1,6 @@
 //console.dir(document)
 //console.log(document.all)
-console.log(document.getElementById('header-title'));
-console.log(document.getElementsByClassName("list-group"))
+//console.log(document.getElementById('header-title'));
+//console.log(document.getElementsByClassName("list-group"))
+var submit=document.querySelector('input[type="submit"]')
+submit.value="send"
