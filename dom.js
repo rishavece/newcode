30991c9a71912b744a4965e04250eb63acfd4f8a
rishavece <rishavk229>
2023-11-21 11:02:00 +0530

    colourchange

--- dom.js
+++ dom.js
@@ -2,5 +2,12 @@
 //console.log(document.all)
 //console.log(document.getElementById('header-title'));
 //console.log(document.getElementsByClassName("list-group"))
-var submit=document.querySelector('input[type="submit"]')
-submit.value="send"
+//var submit=document.querySelector('input[type="submit"]')
+//submit.value="send"
+//var item =document.querySelector('list-group-item');
+//item.style.color='blue';
+
+//var element = document.getElementsByClassName('list-group-item');
+//element.style.color = 'red'; 
+var item=document.querySelector('.list-group-item');
+item.style.color='red';
